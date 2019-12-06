17f6ad368cb5f85acc6fc7bbb5135cc857afae59
Sahil Nare <37807526+sahilnare@users.noreply.github.com>
2019-12-06 19:01:22 +0530

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 function Message(props) {
+    
     return (
+        
         <div className="message">
             <div className="message-username">{props.userName}</div>
             <div className="message-text">{props.text}</div>
         </div>
+        
     )
 }
 
